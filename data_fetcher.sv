e1a260c472e07851435b68a94e7c5c03aae8b163
Neng Chai <chaineng66@outlook.com>
2022-05-03 23:19:16 -0400

    Merge branch 'main' of https://github.com/SrikarVanavasam/systolic-array into main

--- data_fetcher.sv
+++ data_fetcher.sv
@@ -30,19 +30,23 @@ reg [31:0] pointer;
 always @ (posedge clk or posedge reset) begin
     if (reset || !enable) begin
         for (int i = 0; i < MATRIX_SIZE; i++) begin
-            data_w_interval [i] <= 32'b0;
+            data_w_interval[i] <= 32'b0;
         end
         counter <= 0;
         pointer <= 0;
 
-        dmem [0] <= 1;
-        dmem [1] <= 2;
-        dmem [2] <= 3;
-        dmem [3] <= 4;
+        for (int i = 0; i < MATRIX_SIZE*MATRIX_SIZE; i++) begin
+            dmem[i] <= i + 1;
+        end
+
+        // dmem [0] <= 1;
+        // dmem [1] <= 2;
+        // dmem [2] <= 3;
+        // dmem [3] <= 4;
     end
     else if (pointer > MATRIX_SIZE*MATRIX_SIZE - MATRIX_SIZE) begin
         for (int i = 0; i < MATRIX_SIZE; i++) begin
-            data_w_interval [i] <= 32'b0;
+            data_w_interval[i] <= 32'b0;
         end
         counter <= 0;
         pointer <= MATRIX_SIZE*MATRIX_SIZE;
@@ -54,13 +58,13 @@ always @ (posedge clk or posedge reset) begin
     if (counter == 2'b11) begin
         // data_w_interval [MATRIX_SIZE-1:0] <= dmem_out [pointer +: MATRIX_SIZE];
         for (int i = 0; i < MATRIX_SIZE; i++) begin
-            data_w_interval [i] <= dmem [pointer + i];
+            data_w_interval[i] <= dmem[pointer + i];
         end
         pointer <= pointer + MATRIX_SIZE;   // should we use blocking statement for updating the pointer value?
     end
     else begin
         for (int i = 0; i < MATRIX_SIZE; i++) begin
-            data_w_interval [i] <= 32'b0;
+            data_w_interval[i] <= 32'b0;
         end
     end
 
